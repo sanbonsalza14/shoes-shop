9292f713749de95d94b800b87aea0f5bdd609871
804-03 <slamguy14@naver.com>
2025-09-16 16:40:30 +0900

    react-router-dom 설치 및 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,47 @@
-import "./App.css"
-import AppNavBar from './AppNavbar'
-
+import "./App.css";
+import AppNavBar from "./AppNavBar";
 // assets 폴더 내의 이미지 사용법 -> import 해서 사용
 import bg_png from "./assets/images/bg.png"
 import img_2 from "./assets/images/shoes2.jpg"
-import { Container, Row, Col } from 'react-bootstrap';
-import data from  './data/data';
+import {Container, Row, Col} from 'react-bootstrap';
+import data from "./data/data";
 import { useState } from "react";
-
+import Product from "./Product";
 
 function App() {
- //상품정보를 갖는 product 스텡이트를 만든다.
- const [product, setProduct] = useState(data);
- console.log(product)
+  // 상품정보를 갖는 product 스테이트를 만든다.
+  const [product, setProduct] = useState(data);
+
   return (
     <>
-      {/*네비게이션 바 영역시작//*/}
+      {/* 네비게이션 바 영역 시작 */}
       <AppNavBar />
-      {/* //네비게이션 바 영역 끝*/}
+      {/* 네비게이션 바 영역 끝 */}
 
-      {/* //메인 대문사진 영역시작*/}
-      <div className='main-bg' />
-      {/* //메인 대문사진영역 끝 */}
+      {/* 메인 대문사진 영역 시작 */}
+      <div className="main-bg" 
+        style={{backgroundImage: `url('${bg_png}')`}}  
+      />
+      {/* 메인 대문사진 영역 끝 */}
       {/* 상품진열영역 시작 */}
-
-      {/* 상품진열영역 시작 */}
-      <Container fluid>
+      
+      {/* 상품진열영역 끝 */}
+      <Container>
         <Row>
-          <Col className='text-center'>
-           <img src="https://zzzmini.github.io/images/shoes1.jpg" 
-           width="80%"  />
-           <h4>상품명</h4>
-           <p>상품설명</p>
-          </Col>
-           <Col>
-           <img src={img_2}
-           width="80%"  />
-           <h4>상품명</h4>
-           <p>상품설명</p>
-          </Col>
-           <Col>
-           <img src="/imges/shoes3.jpg" 
-           width="80%"  />
-           <h4>상품명</h4>
-           <p>상품설명</p>
-          </Col>
+          {
+            product.map((shoes, _)=>{
+              return(
+                <Col key={shoes.id} className="text-center">
+                  {/* Product 콤포넌트 자리 */}
+                  <Product shoes={shoes}/>
+                </Col>
+              )
+            })
+          }
         </Row>
       </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -1,6 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import './AppNavBar.css'
+//Route : 페이지(컴포넌트) 이동 처리 
+//Routes : url 에 담겨있는 정보를 획득
+//Link : 실제로 페이지를  보여주는 역활, Link위치에 컴포넌트 뿌려줌  
+import { Route, Routes, Link } from 'react-router-dom';
 
 function AppNavbar(){
   return(
@@ -8,14 +12,28 @@ function AppNavbar(){
     <div className='App'>
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand>Muzinjang</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link >
+                <Link to={'/'}>Home</Link>
+                </Nav.Link>
+              <Nav.Link >
+               <Link to={'/detail'}>Home</Link>
+                </Nav.Link>
+              <Nav.Link >
+                <Link to={'/cart'}>Home</Link>
+                </Nav.Link>
             </Nav>
           </Container>
         </Navbar>
+
+      {/*라우팅 정보를 한꺼번에 모아놓는 장소  */}
+      {/* 스프링에서 사용하는 컨투롤러 클래스 */}
+      <Routes>
+        <Route path='/'element={<div>메인페이지</div>}/>
+        <Route path='/detail'element={<div>상세페이지</div>}/>
+        <Route path='/cart'element={<div>장바구니페이지</div>}/>
+      </Routes>
       </div>
     </>
   )
